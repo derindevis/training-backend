c7578696489d339f6f1dd76dc6eaa0630c0bc2ed
derindevis <derindevis79@gmail.com>
2026-05-11 10:58:39 +0530

    added

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm  import sessionmaker,declarative_base
 
-DATABASE_URL=os.getenv('DATABASE_URL',"sqlite:///./kuppam.db")
+DATABASE_URL=os.getenv('DATABASE_URL',"sqlite:///./students.db")
 
 engine=create_engine(
     DATABASE_URL,
